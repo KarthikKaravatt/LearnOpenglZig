fbb7286fe5b9a63cf00d8da7e0135cba4dad555a
KK <82811712+KarthikKaravatt@users.noreply.github.com>
2024-12-19 21:27:45 +0800

    Completed up to 4.6

--- src/main.zig
+++ src/main.zig
@@ -1,21 +1,37 @@
-//! By convention, main.zig is where your main function lives in the case that
 const std = @import("std");
 const glfw = @import("zglfw");
+const zopengl = @import("zopengl");
+
+fn frameBufferSizeCallback(_: *glfw.Window, width: i32, height: i32) callconv(.c) void {
+    zopengl.bindings.viewport(0, 0, width, height);
+}
+
+fn processInput(window: *glfw.Window) void {
+    if (glfw.Window.getKey(window, glfw.Key.escape) == glfw.Action.press) {
+        glfw.Window.setShouldClose(window, true);
+    }
+}
 
 pub fn main() !void {
     try glfw.init();
-    defer glfw.terminate();
-
-    const window = try glfw.Window.create(600, 600, "zig-gamedev: minimal_glfw_gl", null);
-    defer window.destroy();
-
-    // setup your graphics context here
-
-    while (!window.shouldClose()) {
-        glfw.pollEvents();
-
-        // render your things here
-
+    const window_width = 800;
+    const window_height = 600;
+    const gl_major = 4;
+    const gl_minor = 0;
+    glfw.windowHintTyped(.context_version_major, gl_major);
+    glfw.windowHintTyped(.context_version_minor, gl_minor);
+    glfw.windowHintTyped(.opengl_profile, .opengl_core_profile);
+    const window = try glfw.Window.create(window_width, window_height, "LearnOpenGL", null);
+    glfw.makeContextCurrent(window);
+    try zopengl.loadCoreProfile(glfw.getProcAddress, gl_major, gl_minor);
+    const gl = zopengl.bindings;
+    gl.clearBufferfv(gl.COLOR, 0, &[_]f32{ 0.2, 0.4, 0.8, 1.0 });
+    gl.viewport(0, 0, window_width, window_height);
+    _ = window.setFramebufferSizeCallback(frameBufferSizeCallback);
+    while (!glfw.Window.shouldClose(window)) {
+        processInput(window);
         window.swapBuffers();
+        glfw.pollEvents();
     }
+    glfw.terminate();
 }
