4766d09f83e2f65b44199bca058cc328421b6c4c
kk <karthikkaravatt@gmail.com>
2025-03-25 14:36:14 +0800

    make some triangles

---
+++ transformations/pyramid-scheme/src/shaders/triangle.fs
@@ -0,0 +1,15 @@
+#version 330 core
+
+out vec4 FragColor;
+
+in vec3 ourColor;
+in vec2 TextCoord;
+
+uniform sampler2D texture1;
+uniform sampler2D texture2;
+
+void main()
+{
+    vec2 TextCoordFlipped = vec2(-TextCoord.x, TextCoord.y);
+    FragColor = mix(texture(texture1, TextCoord), texture(texture2, TextCoordFlipped), 0.2);
+} 
