58c23252574dde942372169c6bde7211277e443f
kk <karthikkaravatt@gmail.com>
2025-03-30 14:45:40 +0800

    lots of 3d cubes

---
+++ coordinate-systems/3D-cubes/src/main.zig
@@ -0,0 +1,314 @@
+const std = @import("std");
+const builtin = @import("builtin");
+
+const glfw = @import("zglfw");
+const zgl = @import("zopengl");
+const zstbi = @import("zstbi");
+const zm = @import("zmath");
+
+const ShaderConstructor = @import("./shader.zig").ShaderConstructor;
+
+const print = std.debug.print;
+const panic = std.debug.panic;
+
+const is_debug = builtin.mode == .Debug;
+
+const gl = zgl.wrapper;
+const math = std.math;
+
+const gl_major = 4;
+const gl_minor = 3;
+
+const triangle_vertex_shader_path = "src/shaders/triangle.vs";
+const triangle_fragment_shader_path = "src/shaders/triangle.fs";
+
+pub fn main() !void {
+    var debug_allocator: std.heap.DebugAllocator(.{}) = .init;
+    defer if (is_debug) {
+        _ = debug_allocator.deinit();
+    };
+    const gpa, _ = gpa: {
+        if (builtin.os.tag == .wasi) break :gpa .{ std.heap.wasm_allocator, false };
+        break :gpa switch (builtin.mode) {
+            .Debug, .ReleaseSafe => .{ debug_allocator.allocator(), true },
+            .ReleaseFast, .ReleaseSmall => .{ std.heap.smp_allocator, false },
+        };
+    };
+    zstbi.init(gpa);
+    defer zstbi.deinit();
+    glfw.init() catch |err| {
+        panic("Failed to initlaise zglfw: {}", .{err});
+    };
+    defer glfw.terminate();
+
+    const window = glfwSetupWindow("Pyramid Scheme");
+    zgl.loadCoreProfile(glfw.getProcAddress, gl_major, gl_minor) catch |err| {
+        panic("Failed to load opengl:{}", .{err});
+    };
+    gl.enable(.depth_test);
+
+    const vertices = [_]f32{
+        -0.5, -0.5, -0.5, 0.0, 0.0,
+        0.5,  -0.5, -0.5, 1.0, 0.0,
+        0.5,  0.5,  -0.5, 1.0, 1.0,
+        0.5,  0.5,  -0.5, 1.0, 1.0,
+
+        -0.5, 0.5,  -0.5, 0.0, 1.0,
+        -0.5, -0.5, -0.5, 0.0, 0.0,
+        -0.5, -0.5, 0.5,  0.0, 0.0,
+        0.5,  -0.5, 0.5,  1.0, 0.0,
+
+        0.5,  0.5,  0.5,  1.0, 1.0,
+        0.5,  0.5,  0.5,  1.0, 1.0,
+        -0.5, 0.5,  0.5,  0.0, 1.0,
+        -0.5, -0.5, 0.5,  0.0, 0.0,
+
+        -0.5, 0.5,  0.5,  1.0, 0.0,
+        -0.5, 0.5,  -0.5, 1.0, 1.0,
+        -0.5, -0.5, -0.5, 0.0, 1.0,
+        -0.5, -0.5, -0.5, 0.0, 1.0,
+
+        -0.5, -0.5, 0.5,  0.0, 0.0,
+        -0.5, 0.5,  0.5,  1.0, 0.0,
+        0.5,  0.5,  0.5,  1.0, 0.0,
+        0.5,  0.5,  -0.5, 1.0, 1.0,
+
+        0.5,  -0.5, -0.5, 0.0, 1.0,
+        0.5,  -0.5, -0.5, 0.0, 1.0,
+        0.5,  -0.5, 0.5,  0.0, 0.0,
+        0.5,  0.5,  0.5,  1.0, 0.0,
+
+        -0.5, -0.5, -0.5, 0.0, 1.0,
+        0.5,  -0.5, -0.5, 1.0, 1.0,
+        0.5,  -0.5, 0.5,  1.0, 0.0,
+        0.5,  -0.5, 0.5,  1.0, 0.0,
+
+        -0.5, -0.5, 0.5,  0.0, 0.0,
+        -0.5, -0.5, -0.5, 0.0, 1.0,
+        -0.5, 0.5,  -0.5, 0.0, 1.0,
+        0.5,  0.5,  -0.5, 1.0, 1.0,
+
+        0.5,  0.5,  0.5,  1.0, 0.0,
+        0.5,  0.5,  0.5,  1.0, 0.0,
+        -0.5, 0.5,  0.5,  0.0, 0.0,
+        -0.5, 0.5,  -0.5, 0.0, 1.0,
+    };
+
+    const cube_positions = [_]zm.Vec{
+        zm.f32x4(0.0, 0.0, 0.0, 0.0),
+        zm.f32x4(2.0, 5.0, -15.0, 0.0),
+        zm.f32x4(-1.5, -2.2, -2.5, 0.0),
+        zm.f32x4(-3.8, -2.0, -12.3, 0.0),
+        zm.f32x4(2.4, -0.4, -3.5, 0.0),
+        zm.f32x4(-1.7, 3.0, -7.5, 0.0),
+        zm.f32x4(1.3, -2.0, -2.5, 0.0),
+        zm.f32x4(1.5, 2.0, -2.5, 0.0),
+        zm.f32x4(1.5, 0.2, -1.5, 0.0),
+        zm.f32x4(-1.3, 1.0, -1.5, 0.0),
+    };
+
+    const vertex_data_size = @sizeOf(@TypeOf(vertices));
+    // Opengl works with raw bytes
+    const vertices_bytes: []const u8 = std.mem.sliceAsBytes(vertices[0..]);
+    const vertices_byte_ptr: ?[*]const u8 = vertices_bytes.ptr;
+
+    var vao: gl.VertexArrayObject = .{ .name = 0 };
+    var vbo: gl.Buffer = .{ .name = 0 };
+
+    defer gl.deleteVertexArray(&vao);
+    defer gl.deleteBuffer(&vbo);
+
+    gl.genVertexArray(&vao);
+    gl.genBuffer(&vbo);
+
+    gl.bindVertexArray(vao);
+
+    gl.bindBuffer(.array_buffer, vbo);
+    gl.bufferData(
+        .array_buffer,
+        vertex_data_size,
+        vertices_byte_ptr,
+        .static_draw,
+    );
+
+    const vertexCoordLocation: gl.VertexAttribLocation = .{ .location = 0 };
+    const vertexTextureLocation: gl.VertexAttribLocation = .{ .location = 1 };
+    gl.vertexAttribPointer(
+        vertexCoordLocation,
+        3,
+        .float,
+        gl.FALSE,
+        5 * @sizeOf(gl.Float),
+        0,
+    );
+    gl.enableVertexAttribArray(vertexCoordLocation);
+
+    gl.vertexAttribPointer(
+        vertexTextureLocation,
+        2,
+        .float,
+        gl.FALSE,
+        5 * @sizeOf(gl.Float),
+        3 * @sizeOf(gl.Float),
+    );
+    gl.enableVertexAttribArray(vertexTextureLocation);
+
+    var image_1 = zstbi.Image.loadFromFile("src/textures/wall.jpg", 0) catch |err| {
+        panic("Image reading failed:{any}", .{err});
+    };
+    var image_2 = zstbi.Image.loadFromFile("src/textures/awesomeface.png", 0) catch |err| {
+        panic("Image reading failed:{any}", .{err});
+    };
+    defer image_1.deinit();
+    defer image_2.deinit();
+    var texture_1: gl.Texture = .{ .name = 0 };
+    var texture_2: gl.Texture = .{ .name = 0 };
+    gl.genTexture(&texture_1);
+    gl.bindTexture(.texture_2d, texture_1);
+    gl.texParameteri(.texture_2d, .wrap_s, gl.REPEAT);
+    gl.texParameteri(.texture_2d, .wrap_t, gl.REPEAT);
+    gl.texParameteri(.texture_2d, .min_filter, gl.LINEAR_MIPMAP_LINEAR);
+    gl.texParameteri(.texture_2d, .mag_filter, gl.LINEAR);
+    gl.texImage2D(.{
+        .target = .texture_2d,
+        .level = 0,
+        .internal_format = .rgb,
+        .width = image_1.width,
+        .height = image_1.height,
+        .format = .rgb,
+        .pixel_type = .unsigned_byte,
+        .data = image_1.data.ptr,
+    });
+    gl.generateMipmap(.texture_2d);
+    gl.genTexture(&texture_2);
+    gl.bindTexture(.texture_2d, texture_2);
+    gl.texParameteri(.texture_2d, .wrap_s, gl.REPEAT);
+    gl.texParameteri(.texture_2d, .wrap_t, gl.REPEAT);
+    gl.texParameteri(.texture_2d, .min_filter, gl.LINEAR_MIPMAP_LINEAR);
+    gl.texParameteri(.texture_2d, .mag_filter, gl.LINEAR);
+    gl.texImage2D(.{
+        .target = .texture_2d,
+        .level = 0,
+        .internal_format = .rgb,
+        .width = image_2.width,
+        .height = image_2.height,
+        .format = .rgba,
+        .pixel_type = .unsigned_byte,
+        .data = image_2.data.ptr,
+    });
+    gl.generateMipmap(.texture_2d);
+
+    const shader: ShaderConstructor = ShaderConstructor.init(
+        triangle_vertex_shader_path,
+        triangle_fragment_shader_path,
+        gpa,
+    );
+    shader.use();
+    defer shader.deinit();
+    if (hasGlError()) return;
+    shader.setInt("texture1", 0, gpa);
+    shader.setInt("texture2", 1, gpa);
+    while (!window.shouldClose()) {
+        processInput(window);
+        gl.clearColor(0.2, 0.3, 0.3, 1.0);
+        gl.clear(.{ .color = true, .depth = true });
+        if (hasGlError()) return;
+        gl.activeTexture(.texture_0);
+        gl.bindTexture(.texture_2d, texture_1);
+        gl.activeTexture(.texture_1);
+        gl.bindTexture(.texture_2d, texture_2);
+        shader.use();
+        var view = zm.identity();
+        var projection = zm.identity();
+
+        view = zm.mul(view, zm.translation(0.0, 0.0, -3.0));
+        projection = zm.perspectiveFovRhGl(math.degreesToRadians(45.0), 800.0 / 600.0, 0.1, 100.0);
+
+        const viewLoc = gl.getUniformLocation(
+            shader.shaderProgram,
+            "view",
+        ) orelse {
+            panic("view uniform not found", .{});
+        };
+        const porjectionLoc = gl.getUniformLocation(
+            shader.shaderProgram,
+            "projection",
+        ) orelse {
+            panic("projection uniform not found", .{});
+        };
+
+        gl.uniformMatrix4fv(viewLoc, 1, false, &zm.matToArr(view));
+        gl.uniformMatrix4fv(porjectionLoc, 1, false, &zm.matToArr(projection));
+
+        var model = zm.identity();
+        gl.bindVertexArray(vao);
+        for (0..10) |i| {
+            const time: f32 = @floatCast(glfw.getTime());
+            const current_angle = std.math.degreesToRadians(50) * (time * 0.5);
+
+            const rotation_axis = zm.f32x4(0.5, 1.0, 0.0, 0.0);
+            const rotation_matrix = zm.matFromAxisAngle(rotation_axis, current_angle);
+
+            model = zm.mul(model, zm.translationV(cube_positions[i]));
+            model = zm.mul(model, rotation_matrix);
+            shader.setMat4("model", &zm.matToArr(model));
+            gl.drawArrays(.triangles, 0, 36);
+        }
+
+        if (hasGlError()) return;
+        window.swapBuffers();
+        glfw.pollEvents();
+        if (hasGlError()) return;
+    }
+}
+
+fn frameBufferSizeCallback(
+    _: *glfw.Window,
+    width: i32,
+    height: i32,
+) callconv(.c) void {
+    gl.viewport(0, 0, @bitCast(width), @bitCast(height));
+}
+
+fn glfwSetupWindow(title: [:0]const u8) *glfw.Window {
+    glfw.windowHint(.context_version_major, gl_major);
+    glfw.windowHint(.context_version_minor, gl_minor);
+    glfw.windowHint(.resizable, false);
+    if (is_debug) {
+        glfw.windowHint(.opengl_debug_context, true);
+    }
+    if (builtin.target.os.tag == .macos) {
+        glfw.windowHint(.opengl_forward_compat, true);
+    } else {
+        glfw.windowHint(.opengl_profile, .opengl_core_profile);
+    }
+    const window_width = 800;
+    const window_height = 600;
+    const window = glfw.Window.create(
+        window_width,
+        window_height,
+        title,
+        null,
+    ) catch |err| {
+        panic("Window creation failed: {any}", .{err});
+    };
+    glfw.makeContextCurrent(window);
+    _ = window.setFramebufferSizeCallback(frameBufferSizeCallback);
+    return window;
+}
+
+fn hasGlError() bool {
+    if (is_debug) {
+        const e = gl.getError();
+        if (e != gl.Error.no_error) {
+            return true;
+        }
+    }
+    return false;
+}
+
+fn processInput(window: *glfw.Window) void {
+    if (window.getKey(glfw.Key.escape) == glfw.Action.press) {
+        window.setShouldClose(true);
+    }
+}
---
+++ coordinate-systems/3D-cubes/src/shader.zig
@@ -0,0 +1,182 @@
+const std = @import("std");
+const builtin = @import("builtin");
+const zopengl = @import("zopengl");
+
+const print = std.debug.print;
+const panic = std.debug.panic;
+const gl = zopengl.wrapper;
+
+const isDebug = builtin.mode == .Debug;
+const maxShaderSize = 1000000;
+
+pub const ShaderConstructor = struct {
+    shaderProgram: gl.Program,
+
+    pub fn deinit(self: ShaderConstructor) void {
+        gl.deleteProgram(self.shaderProgram);
+    }
+    pub fn init(
+        vertexShaderPath: [:0]const u8,
+        fragmentShaderPath: [:0]const u8,
+        allocator: std.mem.Allocator,
+    ) ShaderConstructor {
+        const vertexShaderSource = readShaderFile(
+            vertexShaderPath,
+            maxShaderSize,
+            allocator,
+        );
+        defer allocator.free(vertexShaderSource);
+        const fragmentShaderSource = readShaderFile(
+            fragmentShaderPath,
+            maxShaderSize,
+            allocator,
+        );
+        defer allocator.free(fragmentShaderSource);
+
+        const vertexShader = createShader(vertexShaderSource, .vertex, allocator);
+        defer gl.deleteShader(vertexShader);
+        const fragmentShader = createShader(fragmentShaderSource, .fragment, allocator);
+        defer gl.deleteShader(fragmentShader);
+
+        const shaderProgram = gl.createProgram();
+        gl.attachShader(shaderProgram, vertexShader);
+        gl.attachShader(shaderProgram, fragmentShader);
+        gl.linkProgram(shaderProgram);
+
+        if (isDebug) {
+            const success = gl.getProgramiv(shaderProgram, .link_status);
+            if (success == 0) {
+                const buffer = allocator.alloc(u8, 512) catch |err|
+                    {
+                        panic("Error allocating buffer:{?}\n", .{err});
+                    };
+                defer allocator.free(buffer);
+                const log = gl.getProgramInfoLog(shaderProgram, buffer);
+                print("ERROR::SHADER::PROGRAM::LINKING_FAILED\n{?s}\n", .{log});
+            } else {
+                const buffer = allocator.alloc(u8, 512) catch |err|
+                    {
+                        panic("Error allocating buffer:{?}\n", .{err});
+                    };
+                defer allocator.free(buffer);
+                print("INFO::SHADER::PROGRAM::LINKING_SUCCESS\n", .{});
+            }
+        }
+        return ShaderConstructor{ .shaderProgram = shaderProgram };
+    }
+    pub fn use(self: ShaderConstructor) void {
+        gl.useProgram(self.shaderProgram);
+    }
+
+    //TODO:Implement
+
+    // pub fn setBool(name: []const u8, value: bool) void {
+    //     gl.uniform1i(gl.getUniformLocation(.ID, name), @intFromBool(value));
+    // }
+    pub fn setInt(
+        slef: ShaderConstructor,
+        name: []const u8,
+        value: gl.Int,
+        allocator: std.mem.Allocator,
+    ) void {
+        const sentiel_str: [:0]u8 = std.mem.Allocator.dupeZ(allocator, u8, name) catch |err| {
+            panic("Error processing uniform name: {any}", .{err});
+        };
+        defer allocator.free(sentiel_str);
+        const location = gl.getUniformLocation(slef.shaderProgram, sentiel_str) orelse {
+            panic("Error accessing uniform location", .{});
+        };
+        gl.uniform1i(location, value);
+    }
+    // pub fn setFloat(name: []const u8, value: gl.Float) void {
+    //     gl.uniform1i(gl.getUniformLocation(.ID, name), value);
+    // }
+    pub fn setMat4(
+        self: ShaderConstructor,
+        name: []const u8,
+        value_ptr: *const [16]f32,
+    ) void {
+        var name_buffer: [64]u8 = undefined;
+        const terminated_name = std.fmt.bufPrintZ(&name_buffer, "{s}", .{name}) catch {
+            std.debug.print("Error: Uniform name '{s}' too long for buffer.\n", .{name});
+            return;
+        };
+        const location: gl.UniformLocation = gl.getUniformLocation(
+            self.shaderProgram,
+            terminated_name,
+        ) orelse {
+            panic("Unifrom location not found'n", .{});
+        };
+        if (location.location == -1) {
+            std.debug.print(
+                \\Warning: Uniform '{s}' not found or inactive in shader program {}.
+            , .{ name, self.program_id });
+            return;
+        }
+        gl.uniformMatrix4fv(
+            location,
+            1,
+            false,
+            value_ptr,
+        );
+    }
+};
+
+pub fn readShaderFile(shaderPath: []const u8, maxSize: u64, allocator: std.mem.Allocator) [:0]const u8 {
+    const cwd = std.fs.cwd();
+    const fileStr = cwd.readFileAlloc(allocator, shaderPath, maxSize) catch |err| {
+        panic("Error reading file: {?} \n", .{err});
+    };
+    defer allocator.free(fileStr);
+    const nullTerminated = std.mem.Allocator.dupeZ(allocator, u8, fileStr) catch |err| {
+        panic("Error copying file:{?} \n", .{err});
+    };
+    return nullTerminated;
+}
+
+fn createShader(
+    shaderSource: [:0]const u8,
+    shaderType: gl.ShaderType,
+    allocator: std.mem.Allocator,
+) gl.Shader {
+    const shader = gl.createShader(shaderType);
+
+    // Create one-element arrays for the source pointer and its length.
+    const src_ptrs = &[_][*:0]const u8{shaderSource};
+    const shaderSourceLen: u32 = @intCast(shaderSource.len);
+    const src_lengths = &[_]u32{shaderSourceLen};
+
+    // Set the shader source.
+    gl.shaderSource(shader, src_ptrs[0..], src_lengths[0..]);
+
+    // Compile the shader.
+    gl.compileShader(shader);
+
+    // Check for compilation errors.
+    if (checkShaderError(shader, allocator)) {
+        panic("Shader failed", .{});
+    }
+
+    return shader;
+}
+
+fn checkShaderError(
+    shader: gl.Shader,
+    allocator: std.mem.Allocator,
+) bool {
+    if (isDebug) {
+        const success = gl.getShaderiv(shader, .compile_status);
+        if (success == 0) {
+            const buffer = allocator.alloc(u8, 512) catch |err|
+                {
+                    panic("Error:{?}\n", .{err});
+                };
+            defer allocator.free(buffer);
+            const log = gl.getShaderInfoLog(shader, buffer);
+            print("ERROR::SHADER::OPERATION_FAILED\n{?s}\n", .{log});
+            return true;
+        }
+    }
+    print("INFO::SHADER::OPERATION_SUCCESS\n", .{});
+    return false;
+}
